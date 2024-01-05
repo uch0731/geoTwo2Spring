4ca85361c73aa8a1aed06ef6aae870a38e7f7273
uch0731 <uch0731@gmail.com>
2024-01-05 15:08:44 +0900

    first commit

---
+++ src/main/java/uch/geotwo2spring/GeoTwo2SpringApplication.java
@@ -0,0 +1,13 @@
+package uch.geotwo2spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoTwo2SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeoTwo2SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uch/geotwo2spring/GeoTwo2SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package uch.geotwo2spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoTwo2SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
