3c1df6b718cd407f8c50edccebf4b69b913e98b7
uch0731 <uch0731@gmail.com>
2024-01-08 15:25:36 +0900

    컨트롤러 분리

---
+++ src/main/java/uch/geotwo2spring/controller/RegionController.java
@@ -0,0 +1,33 @@
+package uch.geotwo2spring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import uch.geotwo2spring.dto.LardAdmSectSggDto;
+import uch.geotwo2spring.service.LardAdmSectSggService;
+
+import java.util.List;
+
+@RestController
+public class RegionController {
+
+   LardAdmSectSggService lardAdmSectSggService;
+
+   @Autowired
+   public RegionController(LardAdmSectSggService lardAdmSectSggService) {
+       this.lardAdmSectSggService = lardAdmSectSggService;
+   }
+
+    //지역 데이터를 모두 보여주는 메서드
+    @GetMapping("/all/region")
+    public List<LardAdmSectSggDto> showAllRegion() {
+        return lardAdmSectSggService.showRegions();
+    }
+
+    //학교 데이터를 입력받아 학교가 속한 지역을 알려주는 메서드
+    @GetMapping("/{schoolName}/region")
+    public List<LardAdmSectSggDto> getRegionBySchool(@PathVariable String schoolName) {
+        return lardAdmSectSggService.getRegionBySchool(schoolName);
+    }
+}
--- src/main/java/uch/geotwo2spring/controller/SchoolController.java
+++ src/main/java/uch/geotwo2spring/controller/SchoolController.java
@@ -1,48 +1,29 @@
 package uch.geotwo2spring.controller;
 
-import org.locationtech.jts.geom.Coordinate;
-import org.locationtech.jts.geom.Geometry;
-import org.locationtech.jts.geom.GeometryFactory;
-import org.locationtech.jts.io.ParseException;
-import org.locationtech.jts.io.WKTReader;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import uch.geotwo2spring.dto.ElemSchoolDto;
 import uch.geotwo2spring.dto.LardAdmSectSggDto;
-import uch.geotwo2spring.entity.ElemSchoolData;
-import uch.geotwo2spring.entity.LardAdmSectSgg;
 import uch.geotwo2spring.service.ElemSchoolDataService;
 import uch.geotwo2spring.service.LardAdmSectSggService;
 
-
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-public class ShowController {
+public class SchoolController {
     private final ElemSchoolDataService elemSchoolDataService;
-    private final LardAdmSectSggService lardAdmSectSggService;
 
     @Autowired
-    public ShowController(ElemSchoolDataService elemSchoolDataService, LardAdmSectSggService lardAdmSectSggService) {
+    public SchoolController(ElemSchoolDataService elemSchoolDataService) {
         this.elemSchoolDataService = elemSchoolDataService;
-        this.lardAdmSectSggService = lardAdmSectSggService;
     }
 
     //초등학교 데이터를 모두 보여주는 메서드
     @GetMapping("/all/elemschool")
     public List<ElemSchoolDto> showAllElemSchool() {
         return elemSchoolDataService.showSchools();
-    }
-
-    //지역 데이터를 모두 보여주는 메서드
-    @GetMapping("/all/region")
-    public List<LardAdmSectSggDto> showAllRegion() {
-        return lardAdmSectSggService.showRegions();
     }
 
     //지역 이름을 입력받아 해당 지역의 초등학교 데이터를 보여주는 메서드
